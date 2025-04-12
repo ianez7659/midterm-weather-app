392d543e89f3ad74e23a6e3e8487ad603813ee70
Yugo Matsuda <yugo1997zazen@gmail.com>
2025-04-11 19:41:02 -0700

    Setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   srcDir: './src',
   outDir: './dist',
   publicDir: './public',
+  integrations: [tailwind()],
   
   build: {
     format: 'directory'
