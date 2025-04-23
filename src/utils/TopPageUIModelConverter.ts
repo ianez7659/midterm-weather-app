e42cda81a60bd152657baf17acf8c3b9b62cc240
Ian <ianez7659@gmail.com>
2025-04-23 11:57:31 -0700

    Update image paths in TopPageUIModelConverter to use absolute URLs

--- src/utils/TopPageUIModelConverter.ts
+++ src/utils/TopPageUIModelConverter.ts
@@ -11,10 +11,10 @@ import { LocationResponse } from "../responses/LocationResponse";
 import { extractTimeOnly } from "../utils/TimeUtils";
 import { escape } from "querystring";
 
-const bgSunnyImagePath = "src/assets/backgroundimages/bg-sunny.png";
-const bgCloudyImagePath = "src/assets/backgroundimages/bg-cloudy.png";
-const bgRainyImagePath = "src/assets/backgroundimages/bg-rainy.png";
-const bgSnowImagePath = "src/assets/backgroundimages/bg-snow.png";
+const bgSunnyImagePath = "/backgroundimages/bg-sunny.png";
+const bgCloudyImagePath = "/backgroundimages/bg-cloudy.png";
+const bgRainyImagePath = "/backgroundimages/bg-rainy.png";
+const bgSnowImagePath = "/backgroundimages/bg-snow.png";
 
 const weatherCodeToBackgroundImage = (code: number): string => {
   const weatherMap: { [key: number]: string } = {
@@ -76,13 +76,13 @@ const weatherCodeToWeather = (code: number): string => {
   return weatherMap[code] || "Unknown";
 };
 
-const iconCloudyImagePath = "src/assets/icons/weather-cloudy.svg";
-const iconFogImagePath = "src/assets/icons/weather-fog.svg";
-const iconPartlyCloudyImagePath = "src/assets/icons/weather-partly cloudy.svg";
-const iconRainImagePath = "src/assets/icons/weather-rain.svg";
-const iconSnowImagePath = "src/assets/icons/weather-snow.svg";
-const iconSunnyImagePath = "src/assets/icons/weather-sunny.svg";
-const iconThunderstormImagePath = "src/assets/icons/weather-thunderstorm.svg";
+const iconCloudyImagePath = "/icons/weather-cloudy.svg";
+const iconFogImagePath = "/icons/weather-fog.svg";
+const iconPartlyCloudyImagePath = "/icons/weather-partly cloudy.svg";
+const iconRainImagePath = "/icons/weather-rain.svg";
+const iconSnowImagePath = "/icons/weather-snow.svg";
+const iconSunnyImagePath = "/icons/weather-sunny.svg";
+const iconThunderstormImagePath = "/icons/weather-thunderstorm.svg";
 
 const weatherCodeToIconImage = (code: number): string => {
   const weatherMap: { [key: number]: string } = {
